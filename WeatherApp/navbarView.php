86e2a821b7aa755f0485119cd5bf1c0683ad541a
daniel <daniel@growlingflea.com>
2019-06-05 15:37:40 -0700

    made new fix

--- WeatherApp/navbarView.php
+++ WeatherApp/navbarView.php
@@ -18,7 +18,7 @@
                 <span class="icon-bar"></span>
                 <span class="icon-bar"></span>
             </button>
-            <a class="navbar-brand" href="#">What Should I Drinks???</a>
+            <a class="navbar-brand" href="#">What Should I Drink???</a>
         </div>
         <div class="navbar-collapse collapse">
             <ul class="nav navbar-nav">
